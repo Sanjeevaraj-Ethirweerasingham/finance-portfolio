fabf095020acb61ba461235b1c5de9bf7c20d3b2
Sanjeevaraj-Ethirweerasingham <129676501+Sanjeevaraj-Ethirweerasingham@users.noreply.github.com>
2025-11-25 21:17:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,10 @@
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // This ensures assets load correctly on GitHub Pages
+  base: './', 
+})
